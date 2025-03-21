e1afd3438376b671d506851c1b348a702a54a88b
Neto Russo <netoacrn1@gmail.com>
2025-03-21 17:56:57 -0300

    Adicionando o modulo de calculo para retangulos

---
+++ js/modulos/retangulo.js
@@ -0,0 +1,14 @@
+function areaRetangulo (a, b) {
+  return a * b
+}
+
+function perimetroRetangulo (a, b) {
+  return 2 * (a + b)
+}
+
+const retangulo = {
+  areaRetangulo,
+  perimetroRetangulo
+}
+
+export default retangulo
